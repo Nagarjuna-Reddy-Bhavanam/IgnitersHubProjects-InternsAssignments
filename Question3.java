acf1d00f4424f8193e26c391d91946de9ef635d0
Nagarjuna-Reddy-Bhavanam <134576802+Nagarjuna-Reddy-Bhavanam@users.noreply.github.com>
2024-01-19 20:50:13 +0530

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -9,16 +9,16 @@ import javax.activation.*;
 public class Question3  
 {  
  public static void main(String [] args){  
-      String to = "bnreddymech@gmail.com";//change accordingly  
+      String to = "hr@ignitershub.com";
       String from = "bnreddy29699@gmail.com";
-      String host = "192.168.29.41";//or IP address  
+      String host = "8080";  
   
-     //Get the session object  
+   
       Properties properties = System.getProperties();  
       properties.setProperty("mail.smtp.host", host);  
       Session session = Session.getDefaultInstance(properties);  
   
-     //compose the message  
+      
       try{  
          MimeMessage message = new MimeMessage(session);  
          message.setFrom(new InternetAddress(from));  
@@ -26,7 +26,7 @@ public class Question3
          message.setSubject("Ping");  
          message.setText("Hello, this is example of sending email  ");  
   
-         // Send message  
+         
          Transport.send(message);  
          System.out.println("message sent successfully....");  
   
